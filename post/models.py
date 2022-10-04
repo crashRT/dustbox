75755bdc357eb8d8f9de94fbd85cdb2f77fac7b0
crashRT <crashrt.farbauti@gmail.com>
2022-10-04 12:41:04 +0900

    post model

--- post/models.py
+++ post/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+class PostModel(models.Model):
+    text = models.TextField()
+    posted_at = models.DateTimeField(default=timezone.now)
