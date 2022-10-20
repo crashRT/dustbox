1f16ee96075aa915bceae6e676cb21b960e4945c
crashRT <85057127+crashRT@users.noreply.github.com>
2022-10-20 17:13:19 +0900

    ユーザー名を追加、16進数化選択追加

--- post/models.py
+++ post/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
-
 
 class PostModel(models.Model):
+    user = models.CharField(max_length=20, null=True,
+                            blank=True, help_text="名無しでもOK")
+    user_enc = models.BooleanField(help_text="名前を16進数化するかどうか")
     text = models.TextField()
+    text_enc = models.BooleanField(help_text="投稿内容を16進数化するかどうか")
     posted_at = models.DateTimeField(default=timezone.now)
