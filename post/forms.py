d3fb2a293d4653a01bcd6dbc1918b9f3b4dc4c63
crashRT <crashrt.farbauti@gmail.com>
2022-10-20 22:36:20 +0900

    投稿フォーム整備

--- post/forms.py
+++ post/forms.py
@@ -3,13 +3,16 @@ from django import forms
 
 
 class add_post(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(add_post, self).__init__(*args, **kwargs)
+
     class Meta:
         model = PostModel
         fields = ('user', 'user_enc', 'text', 'text_enc', 'posted_at')
         widgets = {'posted_at': forms.HiddenInput()}
         labels = {
             'user': '名前',
-            'user_enc': '名前を16進数化する',
+            'user_enc': '名前を16進数化',
             'text': '本文',
-            'text_enc': '本文を16進数化する',
+            'text_enc': '本文を16進数化',
         }
