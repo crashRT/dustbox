a07adcd74f009ebc42f648b256bef822804b0795
crashRT <85057127+crashRT@users.noreply.github.com>
2022-10-20 18:12:44 +0900

    register model to admin

--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import PostModel
+
+
+@admin.register(PostModel)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user')
+    list_display_links = ('id', 'user')
