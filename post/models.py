b3726ad4f53f0b093337e7668607e843a3752ecb
crashRT <85057127+crashRT@users.noreply.github.com>
2022-10-20 17:37:47 +0900

    DBに直接16進数化したものをいれることにする

--- post/models.py
+++ post/models.py
@@ -5,7 +5,5 @@ from django.utils import timezone
 class PostModel(models.Model):
     user = models.CharField(max_length=20, null=True,
                             blank=True, help_text="名無しでもOK")
-    user_enc = models.BooleanField(help_text="名前を16進数化するかどうか")
     text = models.TextField()
-    text_enc = models.BooleanField(help_text="投稿内容を16進数化するかどうか")
     posted_at = models.DateTimeField(default=timezone.now)
