d123a3c0fc00eb004b2e4569f10d1e6c19488ca6
crashRT <85057127+crashRT@users.noreply.github.com>
2023-02-21 02:18:05 +0900

    CSSが反映されないバグを修正

--- dustbox/urls.py
+++ dustbox/urls.py
@@ -17,8 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles import views
+from django.urls import re_path
+from django.views.static import serve 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('post.urls')),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
